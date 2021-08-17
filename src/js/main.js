002548002f1b3a72830cbbf0e27e22cde2a877d2
Jefferson Paz <72327620+jefferson998@users.noreply.github.com>
2021-08-16 23:41:13 -0500

    cambios de 16/8

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,25 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+bottonmToTop = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    bottonmToTop.style.display = "block";
+  } else {
+    bottonmToTop.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
